4c876ced6b4fae09e360e7bea1ab68ebfd5fa2ce
Illia Sokolan <ilsokolan@gmail.com>
2021-09-01 21:21:16 +0200

    timer and homework

--- Js/script.js
+++ Js/script.js
@@ -34,4 +34,48 @@ window.addEventListener('DOMContentLoaded', function() {
         }
     })
 
+    //Timer
+
+    let deadline = '2021-09-05';
+
+    function getTimeRemaining(endtime) {
+        let t = Date.parse(endtime) - Date.parse(new Date()),
+            seconds = Math.floor((t / 1000) % 60),
+            minutes = Math.floor((t / 1000 / 60) % 60),
+            hours = Math.floor(t / 1000 / 60 / 60); 
+
+            return {
+                'total' : t,
+                'hours' : hours,
+                'minutes' : minutes,
+                'seconds' : seconds
+            };
+    }
+
+    function setClock(id, endtime) {
+        let timer = document.getElementById(id),
+            hours = timer.querySelector('.hours'),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds'),
+            timeInterval = setInterval(apdateClock, 1000);
+
+        function apdateClock() {
+            let t = getTimeRemaining(endtime);
+            if (t.hours < 10) hours.textContent = '0' + t.hours;
+            if (t.hours >= 10) hours.textContent = t.hours;
+            if (t.minutes < 10) minutes.textContent = '0' + t.minutes;
+            if (t.minutes >= 10) minutes.textContent = t.minutes;
+            if (t.seconds < 10) seconds.textContent = '0' + t.seconds;
+            if (t.seconds >= 10) seconds.textContent = t.seconds;
+
+            if(t.total <= 0) {
+                clearInterval(timeInterval);
+                seconds.textContent = '00';
+                minutes.textContent = '00';
+                hours.textContent = '00';
+            }
+        }
+    }
+
+    setClock('timer', deadline);
 });
