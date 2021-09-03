feefa2f29cb99818b340561984cfee022f6a8498
Illia Sokolan <ilsokolan@gmail.com>
2021-09-03 14:30:29 +0200

    modal lesson and homework

--- Js/script.js
+++ Js/script.js
@@ -78,4 +78,33 @@ window.addEventListener('DOMContentLoaded', function() {
     }
 
     setClock('timer', deadline);
+
+    //Modal
+
+    let more = document.querySelector('.more'),
+        overlay = document.querySelector('.overlay'),
+        close = document.querySelector('.popup-close'),
+        descBtn = document.querySelectorAll('.description-btn');
+
+    more.addEventListener('click', function() {
+        overlay.style.display = 'block';
+        this.classList.add('more-splash');
+        document.body.style.overflow = 'hidden';
+    })
+
+    
+   
+    descBtn.forEach(function(item) {
+        item.addEventListener('click', function() {
+            overlay.style.display = 'block';
+            document.body.style.overflow = 'hidden';
+        })
+    })
+
+    close.addEventListener('click', function() {
+        overlay.style.display = 'none';
+        more.classList.remove('more-splash');
+        document.body.style.overflow = '';
+    })
+    
 });
